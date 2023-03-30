3d78e90caff2896f5fb6b57df34d3a49c6da6ac2
lcarera <lcarera04@gmail.com>
2023-03-30 12:07:26 -0300

    employee list component

---
+++ src/app/employee/employee-list/employee-list.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { Employee } from '../../shared/models/employee.model';
+import { EmployeeService } from '../employee.service';
+
+@Component({
+  selector: 'app-employee-list',
+  templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.scss']
+})
+export class EmployeeListComponent {
+  employees: Employee[] = [];
+  selectedSortOption: string = 'id'
+  searchTerm: string = '';
+  constructor(private employeeService: EmployeeService) { }
+
+  ngOnInit() {
+    this.getEmployees();
+  }
+
+  getEmployees(): void {
+    this.employeeService.getEmployees()
+      .subscribe(employees => this.employees = employees);
+  }
+
+  sortEmployees(): void {
+    this.employees = this.employees.sort((a:Employee, b:Employee) => {
+      if (a[this.selectedSortOption] < b[this.selectedSortOption]) {
+        return -1;
+      } else if (a[this.selectedSortOption] > b[this.selectedSortOption]) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+  }
+}
