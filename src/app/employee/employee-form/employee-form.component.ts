4e846e7aa567e8c3f98d1620d0adda451f74c6fe
lcarera <lcarera04@gmail.com>
2023-03-31 01:21:48 -0300

    first version of employee form

--- src/app/employee/employee-form/employee-form.component.ts
+++ src/app/employee/employee-form/employee-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-employee-form',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee-form.component.scss']
 })
 export class EmployeeFormComponent implements OnInit {
+  employeeForm: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.employeeForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      jobTitle: ['', Validators.required],
+      department: ['', Validators.required]
+    });
   }
 
+  onSubmit() {
+    console.log(this.employeeForm.value);
+  }
 }
