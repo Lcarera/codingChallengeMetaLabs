754959eb0dbfb761d40b1b8bf7d891bb3399ca7a
lcarera <lcarera04@gmail.com>
2023-03-31 01:21:12 -0300

    added delete method

--- src/app/employee/employee.service.ts
+++ src/app/employee/employee.service.ts
@@ -8,14 +8,19 @@ import { catchError } from 'rxjs/operators';
 })
 export class EmployeeService {
   constructor(private http: HttpClient) {}
-
+  private serverUrl = 'http://localhost:3000/employees'
   getEmployees(): Observable<Employee[]> {
-    return this.http.get<Employee[]>('http://localhost:3000/employees').pipe(
+    return this.http.get<Employee[]>(this.serverUrl).pipe(
       catchError(() => {
         const error = new Error('An error occurred while fetching the employees.');
         console.error(error);
         return throwError(() => error);
       })
     );
+  }
+  
+  deleteEmployee(id: number): Observable<any> {
+    const url = this.serverUrl + id;
+    return this.http.delete(url);
   }
 }
