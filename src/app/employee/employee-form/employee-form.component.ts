bffc619a351df469e0098a6f873ddd4343b5a0a9
lcarera <lcarera04@gmail.com>
2023-03-31 21:33:30 -0300

    added a get employee method if in edit mode

--- src/app/employee/employee-form/employee-form.component.ts
+++ src/app/employee/employee-form/employee-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { EmployeeService } from '../employee.service';
 import { Employee } from 'src/app/shared/models/employee.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-employee-form',
@@ -13,9 +14,14 @@ export class EmployeeFormComponent implements OnInit {
 
   constructor(
     private formBuilder: FormBuilder,
-    private employeeService: EmployeeService) { }
+    private employeeService: EmployeeService,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
+    if (this.route.snapshot.url[0].path == 'edit-employee') {
+     this.getEmployee()
+    }
+   
     this.employeeForm = this.formBuilder.group({
       name: ['', Validators.required],
       jobTitle: ['', Validators.required],
@@ -33,5 +39,22 @@ export class EmployeeFormComponent implements OnInit {
         console.error('Error adding employee: ', error);
       }
     });
+  }
+
+  getEmployee():void{
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    if (id) {
+      this.employeeService.getEmployee(id).subscribe({
+        next: employee => {
+          this.employeeForm.patchValue(employee);
+        },
+        error: error => {
+          console.error('Error getting the employee with id:' + id, error);
+        }
+      });
+    }
+    else{
+      console.log("'" +(this.route.snapshot.paramMap.get('id') + "' it is not a valid Id"));
+    }
   }
 }
